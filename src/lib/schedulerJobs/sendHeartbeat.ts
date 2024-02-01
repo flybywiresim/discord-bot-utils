f7d5b1e4735576c9f312fc9be01d1273a06097c2
Philippe Dellaert <philippe@dellaert.org>
2024-01-31 23:43:08 -0800

    feat: adding heartbeat mechanism for monitoring
    Introduces a heartbeat mechanism, which makes a GET request to a
    heartbeat URL.
    
    Works using the scheduler mechanism:
    On start of the bot, it will check if there is already a heartbeat job
    configured. If not, it creates it if the URL and interval is set. If the
    job exists, it checks if the interval is still the same as before, if
    not, it recreates the job with the new interval.

---
+++ src/lib/schedulerJobs/sendHeartbeat.ts
@@ -0,0 +1,33 @@
+import { Job } from '@hokify/agenda';
+import { Logger, getScheduler } from '../index';
+
+export async function sendHeartbeat(job: Job) {
+    const scheduler = getScheduler();
+    if (!scheduler) {
+        Logger.error('Failed to get scheduler instance');
+        return;
+    }
+
+    // Needed because of https://github.com/agenda/agenda/issues/401
+    // eslint-disable-next-line no-underscore-dangle
+    const matchingJobs = await scheduler.jobs({ _id: job.attrs._id });
+    if (matchingJobs.length !== 1) {
+        Logger.debug('Job has been deleted already, skipping execution.');
+        return;
+    }
+
+    const heartbeatUrl = process.env.HEARTBEAT_URL;
+    if (!heartbeatUrl) {
+        Logger.error('HEARTBEAT_URL environment variable not set');
+        return;
+    }
+    try {
+        const heartBeatResult = await fetch(`${heartbeatUrl}`);
+        if (heartBeatResult.status !== 200) {
+            Logger.error('Failed to send heartbeat:', heartBeatResult.statusText);
+        }
+    } catch (error) {
+        Logger.error('Failed to send heartbeat:', error);
+    }
+    Logger.info('Heartbeat sent successfully');
+}
