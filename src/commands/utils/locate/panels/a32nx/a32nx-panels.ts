38b9053477f267fa1b2e869039c05d41f629e9cb
ExampleWasTaken <58574351+ExampleWasTaken@users.noreply.github.com>
2024-05-15 16:31:54 +0200

    fix panel import

--- src/commands/utils/locate/panels/a32nx/a32nx-panels.ts
+++ src/commands/utils/locate/panels/a32nx/a32nx-panels.ts
@@ -1,8 +1,9 @@
 import { Panel } from '../panel';
 import { accuPressPanel, autobrakeAndGearPanel, clockPanel, dcdu, ewd, instrumentLightingPanel, isis, nd, pfd, sd } from './front-panel';
 import { efisPanel, fcuPanel, lightKnobsPanel, warningPanel } from './glareshield';
-import { antiIcePanel, adirsPanel, apuPanel, callsPanel, cvrPanel, emerElecPwrPanel, evacPanel, extLtPanel, fltCtlPanel, gpwsPanel, intLtPanel, oxyPanel, paVideoPanel, signsPanel, wiperPanel, cabinPressPanel, airCondPanel, elecPanel, fuelPanel, hydPanel, firePanel, engManStartN1ModePanel, ventilationPanel, cargoSmokePanel, cargoVentPanel, thirdACP, readingLightsJumpSeats, cockpitDoorIndicatorPanel, eltPanel, pedestalLightPanel, emerCbPanel, rearBackCbPanel, fmsLoadPanel, maintenancePanel } from './overhead';
+import { antiIcePanel, adirsPanel, apuPanel, callsPanel, cvrPanel, emerElecPwrPanel, evacPanel, extLtPanel, fltCtlPanel, gpwsPanel, intLtPanel, oxyPanel, paVideoPanel, signsPanel, wiperPanel, cabinPressPanel, airCondPanel, elecPanel, fuelPanel, hydPanel, firePanel, engManStartN1ModePanel, ventilationPanel, cargoSmokePanel, cargoVentPanel, thirdACP, readingLightsJumpSeats, cockpitDoorIndicatorPanel, eltPanel, pedestalLightPanel, emerCbPanel, fmsLoadPanel, maintenancePanel } from './overhead';
 import { aidsDfdrPanel, atcTcasPanel, captPedestalLightingPanel, cockpitDoorPanel, console, ecamControlPanel, engPanel, flaps, gravityGearExtensionPanel, mcdu, parkBrkPanel, printer, rmpAcpPanel, rudderTrim, speedBrake, switchingPanel, thrLvrPitchTrim, wxPanel } from './pedestal';
+import { rearBackCbPanel } from './rear-cb-panel';
 
 export const a32nxPanels: Panel[] = [
     // OVERHEAD
