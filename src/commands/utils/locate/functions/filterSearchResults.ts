e718289d2fcc00764b6532489be60a0d5275bcfd
ExampleWasTaken <58574351+ExampleWasTaken@users.noreply.github.com>
2024-05-15 22:16:19 +0200

    optimize search to include more sensible results

--- src/commands/utils/locate/functions/filterSearchResults.ts
+++ src/commands/utils/locate/functions/filterSearchResults.ts
@@ -2,12 +2,23 @@ import { ApplicationCommandOptionChoiceData } from 'discord.js';
 import { Panel } from '../panels/panel';
 
 export const filterSearchResults = (query: string, source: Map<string, Panel>) => {
-    const filteredTargets = Array.from(source.keys()).filter((current) => current.toLowerCase().startsWith(query.toLowerCase()));
+    // Get any target that includes the query string.
+    const possibleTargets = Array.from(source.keys()).filter((current) => current.toLowerCase().includes(query.toLowerCase()));
 
-    filteredTargets.sort((a, b) => a.indexOf(query) - b.indexOf(query));
+    possibleTargets.sort((a, b) => a.indexOf(query) - b.indexOf(query));
 
+    // Remove all targets that are not preceded by a hyphen, or where .substring(0) !== query.
+    const filteredTargets = possibleTargets.filter((current) => {
+        const indexOfQuery = current.indexOf(query);
+
+        if (indexOfQuery === 0) return true;
+
+        return current.charAt(indexOfQuery - 1) === '-';
+    });
+
+    // Limit the number of returned targets to 25. (Discord limitation)
     const choices: ApplicationCommandOptionChoiceData<string | number>[] = [];
-    for (let i = 0; i < Math.min(filteredTargets.length, 25); i++) {
+    for (let i = 0; i < filteredTargets.length && i < 25; i++) {
         choices.push({ name: filteredTargets[i], value: filteredTargets[i] });
     }
 
