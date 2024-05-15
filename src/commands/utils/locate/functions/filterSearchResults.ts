4f821ed6e1bf77b862ef6fd2c6d80201df01e8f0
ExampleWasTaken <58574351+ExampleWasTaken@users.noreply.github.com>
2024-05-15 17:25:31 +0200

    remove benchmarking

--- src/commands/utils/locate/functions/filterSearchResults.ts
+++ src/commands/utils/locate/functions/filterSearchResults.ts
@@ -2,8 +2,6 @@ import { ApplicationCommandOptionChoiceData } from 'discord.js';
 import { Panel } from '../panels/panel';
 
 export const filterSearchResults = (query: string, source: Map<string, Panel>) => {
-    const start = performance.now();
-
     const filteredTargets = Array.from(source.keys()).filter((current) => current.toLowerCase().startsWith(query.toLowerCase()));
 
     filteredTargets.sort((a, b) => a.indexOf(query) - b.indexOf(query));
@@ -12,10 +10,6 @@ export const filterSearchResults = (query: string, source: Map<string, Panel>) =
     for (let i = 0; i < Math.min(filteredTargets.length, 25); i++) {
         choices.push({ name: filteredTargets[i], value: filteredTargets[i] });
     }
-
-    const end = performance.now();
-
-    console.log(`Found ${choices.length} results in ${end - start}ms.`);
 
     return choices;
 };
