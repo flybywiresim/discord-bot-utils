f3bcff523a8fa375aefbbd698cc5f31ed73c1180
ExampleWasTaken <58574351+ExampleWasTaken@users.noreply.github.com>
2024-05-16 03:43:02 +0200

    fix: rear cb has no image

--- src/commands/utils/locate/panels/a32nx/rear-cb-panel.ts
+++ src/commands/utils/locate/panels/a32nx/rear-cb-panel.ts
@@ -6,7 +6,7 @@ export const rearBackCbPanel: Panel = {
     title: 'FlyByWire A32NX | Rear Back Circuit Breaker Panel',
     docsUrl: `${DOCS_BASE_URLS.a32nx.aftOverhead}/circuit/#rear-right-back-panel`,
     flightDeckUrl: DOCS_BASE_URLS.a32nx.flightdeck,
-    imagePath: `${IMAGE_BASE_URLS.a32nx.rearCb}/rear_right_back.png`,
+    imagePath: `${IMAGE_BASE_URLS.a32nx.rearCb}/rear_right_back.jpg`,
     identifiers: [
         'rear-back-circuit-breaker-panel',
         'secondary-circuit-breaker-panel',
