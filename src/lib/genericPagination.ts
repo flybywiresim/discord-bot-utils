d5eb3ae292cae5c583e2941e547a56b266be44ea
ExampleWasTaken <58574351+ExampleWasTaken@users.noreply.github.com>
2024-05-22 05:11:31 +0200

    simplify pagination handler logic

--- src/lib/genericPagination.ts
+++ src/lib/genericPagination.ts
@@ -1,6 +1,6 @@
-import { ActionRowBuilder, ButtonBuilder, ButtonStyle, CommandInteraction, Interaction, InteractionResponse, Message } from 'discord.js';
+import { ActionRowBuilder, ButtonBuilder, ButtonInteraction, ButtonStyle, CommandInteraction, ComponentType, EmbedBuilder, Interaction, InteractionResponse, Message } from 'discord.js';
 
-export async function sendPaginatedEmbed(interaction: CommandInteraction, embeds: any[]): Promise<void> {
+export async function createPaginatedEmbedInteraction(initialInteraction: CommandInteraction, embeds: EmbedBuilder[]): Promise<void> {
     let currentPage = 0;
 
     const nextButton = new ButtonBuilder()
@@ -13,20 +13,22 @@ export async function sendPaginatedEmbed(interaction: CommandInteraction, embeds
         .setLabel('Previous')
         .setStyle(ButtonStyle.Primary);
 
+    setButtonStates();
+
     const buttonRow = new ActionRowBuilder<ButtonBuilder>().addComponents(prevButton, nextButton);
 
     let message: Message<boolean> | InteractionResponse<boolean>;
-    if ((interaction.deferred || interaction.deferred) || (interaction.deferred && interaction.deferred)) {
-        message = await interaction.editReply({ embeds: [embeds[currentPage]], components: [buttonRow] });
+    if (initialInteraction.deferred || initialInteraction.replied) {
+        message = await initialInteraction.editReply({ embeds: [embeds[currentPage]], components: [buttonRow] });
     } else {
-        message = await interaction.reply({ embeds: [embeds[currentPage]], components: [buttonRow] });
+        message = await initialInteraction.reply({ embeds: [embeds[currentPage]], components: [buttonRow] });
     }
 
-    const filter = (buttonInteraction: Interaction) => buttonInteraction.user.id === interaction.user.id;
-    const collector = message.createMessageComponentCollector({ filter, time: 120000 });
+    const filter = (buttonInteraction: Interaction) => initialInteraction.user.id === buttonInteraction.user.id;
+    const collector = message.createMessageComponentCollector({ filter, componentType: ComponentType.Button, time: 120_000 });
 
-    collector.on('collect', async (buttonInteraction: any) => {
-        buttonInteraction.deferUpdate();
+    collector.on('collect', async (collectedInteraction: ButtonInteraction) => {
+        collectedInteraction.deferUpdate();
 
         if (buttonInteraction.customId === 'pagination_nextPage') {
             currentPage++;
@@ -34,16 +36,25 @@ export async function sendPaginatedEmbed(interaction: CommandInteraction, embeds
             currentPage--;
         }
 
-        if (currentPage < 0) {
-            currentPage = 0;
-        } else if (currentPage >= embeds.length) {
-            currentPage = embeds.length - 1;
-        }
+        setButtonStates();
 
-        updateEmbed();
+        updateEmbed(initialInteraction);
     });
 
-    function updateEmbed() {
-        message.edit({ embeds: [embeds[currentPage]], components: [buttonRow] });
+    collector.on('end', async () => {
+        handleEmbedExpire(initialInteraction);
+    });
+
+    function updateEmbed(collectedInteraction: CommandInteraction) {
+        collectedInteraction.editReply({ embeds: [embeds[currentPage]], components: [buttonRow] });
+    }
+
+    function handleEmbedExpire(initialInteraction: CommandInteraction) {
+        initialInteraction.editReply({ embeds: [embeds[currentPage].setFooter({ text: 'This embed has expired.' })], components: [] });
+    }
+
+    function setButtonStates() {
+        prevButton.setDisabled(currentPage <= 0);
+        nextButton.setDisabled(currentPage >= embeds.length - 1);
     }
 }
