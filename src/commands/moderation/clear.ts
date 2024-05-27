d9dd2157fe16959ba0ac9c57976032f626e666a2
LunakisLeaks <lunakisgames@gmail.com>
2024-05-27 14:04:06 +0300

    Updated clear.ts

--- src/commands/moderation/clear.ts
+++ src/commands/moderation/clear.ts
@@ -1,5 +1,5 @@
 import { ApplicationCommandOptionType, ApplicationCommandType, TextChannel, Colors } from 'discord.js';
-import { slashCommand, slashCommandStructure, makeEmbed, constantsConfig } from '../../lib';
+import { slashCommand, slashCommandStructure, makeEmbed, constantsConfig, Logger } from '../../lib';
 
 const data = slashCommandStructure({
     name: 'clear',
@@ -17,46 +17,48 @@ const data = slashCommandStructure({
     }],
 });
 
-export default slashCommand(data, async ({ interaction, log, client }) => {
+export default slashCommand(data, async ({ interaction }) => {
     const amount = interaction.options.getInteger('amount');
 
-    if (!interaction.channel || !interaction.channel.isTextBased()) {
-        return interaction.reply({ content: 'This command can only be used in text channels.', ephemeral: true });
-    }
-
-    if (amount === null) {
+    if (!amount) {
         return interaction.reply({ content: 'Please specify the number of messages to delete.', ephemeral: true });
     }
 
+    const { channel, user } = interaction;
+    if (!channel) {
+        return interaction.reply({ content: 'The channel could not be resolved.', ephemeral: true });
+    }
+
     try {
-        const messages = await interaction.channel.bulkDelete(amount, true);
+        const messages = await (channel as TextChannel).bulkDelete(amount, true);
 
         const replyEmbed = makeEmbed({
             title: 'Messages Deleted',
             description: `Successfully deleted **${messages.size}** messages.`,
             color: Colors.Green,
-            footer: { text: `Requested by ${interaction.user.tag}`, iconURL: interaction.user.displayAvatarURL() },
+            footer: { text: `Requested by ${user.tag}`, iconURL: user.displayAvatarURL() },
             timestamp: new Date(),
         });
 
-        const targetChannel = await client.channels.fetch(constantsConfig.channels.MOD_LOGS);
+        const modLogsChannel = interaction.guild.channels.resolve(constantsConfig.channels.MOD_LOGS) as TextChannel;
 
-        if (targetChannel && (targetChannel instanceof TextChannel)) {
-            const authorEmbed = makeEmbed({
-                title: '🧹 Messages Cleared',
-                description: 'Messages have been cleared.',
-                color: Colors.Red,
-                fields: [
-                    { name: 'Moderator', value: `<@${interaction.user.id}>`, inline: true },
-                    { name: 'Channel', value: `<#${interaction.channel.id}>`, inline: true },
-                    { name: 'Amount', value: `${amount}`, inline: true },
-                ],
-                footer: { text: `Moderator ID: ${interaction.user.id}`, iconURL: interaction.user.displayAvatarURL() },
-                timestamp: new Date(),
-            });
-            await targetChannel.send({ embeds: [authorEmbed] });
-        } else {
-            log('Target channel not found or not a text channel');
+        const modLogEmbed = makeEmbed({
+            title: '🧹 Messages Cleared',
+            description: 'Messages have been cleared.',
+            color: Colors.Red,
+            fields: [
+                { name: 'Moderator', value: `<@${user.id}>`, inline: true },
+                { name: 'Channel', value: `<#${channel.id}>`, inline: true },
+                { name: 'Amount', value: `${amount}`, inline: true },
+            ],
+            footer: { text: `Moderator ID: ${user.id}`, iconURL: user.displayAvatarURL() },
+            timestamp: new Date(),
+        });
+
+        try {
+            await modLogsChannel.send({ embeds: [modLogEmbed] });
+        } catch (e) {
+            Logger.error('An error occurred while trying to send the mod log:', e);
         }
 
         await interaction.reply({ embeds: [replyEmbed], ephemeral: true });
@@ -64,15 +66,15 @@ export default slashCommand(data, async ({ interaction, log, client }) => {
         setTimeout(async () => {
             try {
                 await interaction.deleteReply();
-            } catch (err) {
-                log('Failed to delete the reply message:', err);
+            } catch (error) {
+                Logger.error('Failed to delete the reply message:', error);
                 await interaction.followUp({ content: 'Failed to delete the reply message.', ephemeral: true });
             }
         }, 5000);
 
         return Promise.resolve();
     } catch (error) {
-        log('Error deleting messages:', error);
+        Logger.error('Error deleting messages:', error);
         return interaction.reply({ content: 'There was an error trying to delete messages in this channel.', ephemeral: true });
     }
 });
