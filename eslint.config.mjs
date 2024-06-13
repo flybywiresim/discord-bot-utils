0cf5021bc52a0146eda2fdf9842a16be1c0b06c1
ExampleWasTaken <58574351+ExampleWasTaken@users.noreply.github.com>
2024-06-13 17:52:22 +0200

    update eslint and ts-eslint and add prettier support

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -5,7 +5,7 @@ import prettierConfig from 'eslint-config-prettier';
 
 export default tseslint.config(
     eslint.configs.recommended,
-    ...tseslint.configs.strictTypeChecked,
+    ...tseslint.configs.recommendedTypeChecked,
     prettierConfig,
     {
         plugins: {
