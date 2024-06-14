f082f9a47eae0ffe1757d73239658e16bf91ada0
ExampleWasTaken <58574351+ExampleWasTaken@users.noreply.github.com>
2024-06-14 15:12:33 +0200

    add prettier and improve eslint config

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,21 +1,38 @@
 import eslint from '@eslint/js';
 import tseslint from 'typescript-eslint';
-import prettierPlugin from 'eslint-plugin-prettier';
-import prettierConfig from 'eslint-config-prettier';
+import recommendedConfig from 'eslint-plugin-prettier/recommended';
 
 export default tseslint.config(
     eslint.configs.recommended,
     ...tseslint.configs.recommendedTypeChecked,
-    prettierConfig,
+    recommendedConfig,
     {
-        plugins: {
-            prettierPlugin,
-        },
+        name: 'Main Config',
         languageOptions: {
             parserOptions: {
                 project: true,
                 tsconfigRootDir: import.meta.dirname,
-            }
+            },
         },
-    }
+        rules: {
+            'prettier/prettier': ['error', {}, { usePrettierrc: true }],
+
+            'no-unused-vars': 'off', // Ensure eslint doesn't care about unused variables.
+            '@typescript-eslint/no-unused-vars': [
+                'error',
+                {
+                    varsIgnorePattern: '^_',
+                    argsIgnorePattern: '^_',
+                },
+            ],
+        },
+    },
+    {
+        files: ['**/*.mjs', '**/*.cjs', '**/*.js'],
+        extends: [tseslint.configs.disableTypeChecked],
+    },
+    {
+        // DO NOT ADD ANY PROPERTIES TO THIS OBJECT
+        ignores: ['node_modules', 'build', 'assets'],
+    },
 );
