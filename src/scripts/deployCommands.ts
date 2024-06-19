2f2e5bc00dfa357d7e43fcae2338fa9241d706ad
ExampleWasTaken <58574351+ExampleWasTaken@users.noreply.github.com>
2024-06-19 15:25:00 +0200

    fix lint deployCommands.ts

--- src/scripts/deployCommands.ts
+++ src/scripts/deployCommands.ts
@@ -1,5 +1,5 @@
 import { REST, Routes, APIUser } from 'discord.js';
-import { constantsConfig } from '../lib';
+import { ContextMenuCommand, SlashCommand, constantsConfig } from '../lib';
 
 if (!constantsConfig.guildId) {
   throw new Error('guildId configuratoin constant is not defined.');
@@ -11,7 +11,7 @@ if (!process.env.BOT_SECRET) {
 
 const rest = new REST({ version: '10' }).setToken(process.env.BOT_SECRET);
 
-export async function deployCommands(commandArray: any[], contextArray: any[]) {
+export async function deployCommands(commandArray: SlashCommand[], contextArray: ContextMenuCommand[]) {
   const body = [...commandArray.map((cmd) => cmd.meta), ...contextArray.map((ctx) => ctx.meta)];
   const currentUser = (await rest.get(Routes.user())) as APIUser;
 
