7a0c89bed1cf1e180c0696246d5421b32b483e39
ExampleWasTaken <58574351+ExampleWasTaken@users.noreply.github.com>
2024-07-02 03:21:55 +0200

    refactor fetchData

--- src/lib/apis/fetchData.ts
+++ src/lib/apis/fetchData.ts
@@ -5,33 +5,32 @@ import { ZodSchema } from 'zod';
  * Fetch data from any API endpoint that returns JSON formatted data.
  * @typeParam ReturnType - The expected type of the returned data.
  * @param request The [Request](https://developer.mozilla.org/en-US/docs/Web/API/Request) object to be passed to `fetch()`.
- * @param typeGuard The type guard to ensure the retrieved data is of the expected type. The retrieved data will be converted to JSON and passed as argument to this function.
- * **It is up to the developer to ensure the type guard works correctly!**
+ * @param zodSchema The [Zod](https://github.com/colinhacks/zod) schema that the returned data conforms to. The promise will reject if the returned data does not conform to the schema provided.
  * @returns A promise that resolves to the expected type or rejects with an [Error](https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Error).
  */
-export const fetchData = async <ReturnType>(request: Request, zodSchema: ZodSchema<ReturnType>): Promise<ReturnType> => {
+export const fetchData = async <ReturnType = unknown>(request: Request, zodSchema: ZodSchema<ReturnType>): Promise<ReturnType> => {
     try {
         const response = await fetch(request);
 
         if (!response.ok) {
-            return Promise.reject(new Error(`HTTP Error. Status: ${response.status}`));
+            throw new Error(`HTTP Error. Status: ${response.status}`);
         }
 
         let data;
         try {
             data = await response.json();
         } catch (e) {
-            return Promise.reject(new Error(`Could not parse JSON. Make sure the endpoint at ${request.url} returns valid JSON. Error: ${String(e)}`));
+            throw new Error(`Could not parse JSON. Make sure the endpoint at ${request.url} returns valid JSON. Error: ${String(e)}`);
         }
 
         const result = zodSchema.safeParse(data);
 
         if (!result.success) {
-            return Promise.reject(result.error);
+            throw result.error;
         }
 
-        return Promise.resolve<ReturnType>(result.data);
+        return result.data;
     } catch (e) {
-        return Promise.reject(new Error(`An error occured while fetching data from ${request.url}: ${String(e)}`));
+        throw new Error(`An error occured while fetching data from ${request.url}: ${String(e)}`);
     }
 };
