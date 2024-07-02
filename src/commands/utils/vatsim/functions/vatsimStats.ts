7b37d6ee0664fc17b2786bf84d863e3cc6d5f4d1
ExampleWasTaken <58574351+ExampleWasTaken@users.noreply.github.com>
2024-07-02 02:50:08 +0200

    migrate vatsim stats to zod

--- src/commands/utils/vatsim/functions/vatsimStats.ts
+++ src/commands/utils/vatsim/functions/vatsimStats.ts
@@ -1,54 +1,47 @@
 import { ChatInputCommandInteraction } from 'discord.js';
-import { makeEmbed } from '../../../../lib';
+import { VatsimData, makeEmbed } from '../../../../lib';
 
-const statsEmbed = (pilots: string, controllers: string, atis: string, observers: string, callsign: any) => makeEmbed({
+const statsEmbed = (pilots: number, controllers: number, atis: number, observers: number, callsign?: string) => makeEmbed({
     title: callsign ? `VATSIM Data | Statistics for callsign ${callsign}` : 'VATSIM Data | Statistics',
     description: callsign ? `An overview of the current active Pilots, Controllers, ATIS and Observers matching ${callsign}.` : 'An overview of the current active Pilots, Controllers, ATIS and Observers.',
     fields: [
         {
             name: 'Pilots',
-            value: pilots,
+            value: pilots.toString(),
             inline: true,
         },
         {
             name: 'Controllers',
-            value: controllers,
+            value: controllers.toString(),
             inline: true,
         },
         {
             name: 'ATIS',
-            value: atis,
+            value: atis.toString(),
             inline: true,
         },
         {
             name: 'Observers',
-            value: observers,
+            value: observers.toString(),
             inline: true,
         },
     ],
 });
 
-export async function handleVatsimStats(interaction: ChatInputCommandInteraction<'cached'>, vatsimData: any, callsignSearch: any) {
-    const vatsimAllControllers = vatsimData.controllers ? vatsimData.controllers.filter((controller: { facility: number; }) => controller.facility > 0) : null;
-    const vatsimAllObservers = vatsimData.controllers ? vatsimData.controllers.filter((controller: { facility: number; }) => controller.facility <= 0) : null;
+export async function handleVatsimStats(interaction: ChatInputCommandInteraction<'cached'>, vatsimData: VatsimData, callsignSearch?: string) {
+    const controllers = vatsimData.controllers.filter((controller) => controller.facility > 0);
+    const observers = vatsimData.controllers.filter((controller) => controller.facility <= 0);
+    const { atis } = vatsimData;
+    const { pilots } = vatsimData;
 
     if (!callsignSearch) {
-        const vatsimPilotCount = vatsimData.pilots ? vatsimData.pilots.length : 0;
-        const vatsimControllerCount = vatsimAllControllers ? vatsimAllControllers.length : 0;
-        const vatsimAtisCount = vatsimData.atis ? vatsimData.atis.length : 0;
-        const vatsimObserverCount = vatsimAllObservers ? vatsimAllObservers.length : 0;
-
-        return interaction.reply({ embeds: [statsEmbed(vatsimPilotCount, vatsimControllerCount, vatsimAtisCount, vatsimObserverCount, null)] });
+        return interaction.editReply({ embeds: [statsEmbed(pilots.length, controllers.length, atis.length, observers.length)] });
     }
-    const vatsimPilots = vatsimData.pilots ? vatsimData.pilots.filter((pilot: { callsign: string | string[]; }) => pilot.callsign.includes(callsignSearch)) : null;
-    const vatsimControllers = vatsimAllControllers ? vatsimAllControllers.filter((controller: { callsign: string | string[]; }) => controller.callsign.includes(callsignSearch)) : null;
-    const vatsimAtis = vatsimData.atis ? vatsimData.atis.filter((atis: { callsign: string | string[]; }) => atis.callsign.includes(callsignSearch)) : null;
-    const vatsimObservers = vatsimAllObservers ? vatsimAllObservers.filter((observer: { callsign: string | string[]; }) => observer.callsign.includes(callsignSearch)) : null;
 
-    const vatsimPilotCount = vatsimPilots ? vatsimPilots.length : 0;
-    const vatsimControllerCount = vatsimControllers ? vatsimControllers.length : 0;
-    const vatsimAtisCount = vatsimAtis ? vatsimAtis.length : 0;
-    const vatsimObserverCount = vatsimObservers ? vatsimObservers.length : 0;
+    const matchedControllers = controllers.filter((controller) => controller.callsign.includes(callsignSearch));
+    const matchedObservers = observers.filter((observer) => observer.callsign.includes(callsignSearch));
+    const matchedPilots = pilots.filter((pilot) => pilot.callsign.includes(callsignSearch));
+    const matchedAtis = atis.filter((atis) => atis.callsign.includes(callsignSearch));
 
-    return interaction.reply({ embeds: [statsEmbed(vatsimPilotCount, vatsimControllerCount, vatsimAtisCount, vatsimObserverCount, callsignSearch)] });
+    return interaction.editReply({ embeds: [statsEmbed(matchedPilots.length, matchedControllers.length, matchedAtis.length, matchedObservers.length, callsignSearch)] });
 }
